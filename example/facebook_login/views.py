2a556b60eb1795caabb7a880640b1b9adad0cfc8
leesangyoon <pinetree408@gmail.com>
2017-04-09 15:56:50 +0900

    Add login logic at views of example app

--- example/facebook_login/views.py
+++ example/facebook_login/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from django.views.generic import View, TemplateView
+
+from django_facebook_auth.views import FacebookLoginMixin
+from django_facebook_auth.models import FacebookSession
+
+
+class ExampleUserDetail(TemplateView):
+    template_name = "example_user_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ExampleUserDetail, self).get_context_data(**kwargs)
+        facebook_session = FacebookSession.objects.get(user_id=self.request.user.pk)
+        user = facebook_session.user
+        profile = facebook_session.query('me')
+
+        context['username'] = user.username
+        context['facebookid'] = profile['id']
+
+        return context
+
+
+class ExampleLogin(FacebookLoginMixin, View):
+    template_name = "example_login.html"
+
+
+class ExampleLogout(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect('/')
